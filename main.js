533386baff60c0aff33c98adeb6028c04f44a2b7
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-08-24 11:07:41 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const svgObject = document.getElementById("the-svg");
+
+//   svgObject.addEventListener("load", function() {
+//     const svgDoc = svgObject.contentDocument;
+    
+//     // grab cls-4 to confirm load
+//     const outline = svgDoc.querySelector(".cls-4");
+    
+//     if( outline ) {
+//       // inject style sheet for the svg
+//       const style = svgDoc.createElementNS()
+//     }
+    
+//   });
+// });
+
+
+// theApp.innerHTML = `
+
+// `
+
