cc64a62f61a673c26b361b30a4a2f96e2e8133da
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-09-03 12:10:44 -0400

    webflow link working with labels and info as well

--- main.js
+++ main.js
@@ -3,23 +3,20 @@ import * as d3 from 'd3';
 
 const overlay = 'https://cdn.prod.website-files.com/66c4bc9a1e606660c92d9d24/66d5b8ff4aaadc8d13ce8c78_interact.svg';
 
-  
+// get the card from the DOM
+const card = document.querySelector(".info-card-mplace");
+
 d3.xml( overlay )
   .then(data => {
     const svg = data.documentElement;
     svg.id = "overlay-item"; // Assign an ID or any attributes you need
     
-    // get the info
-    const info = getInfo();
-    console.log("chweck it out: ", info);
-    
     // add the svg to the DOM
     const svgContainer = document.querySelector(".svg-container-mplace");
-    console.log("the webflow container", svgContainer);
+    
+    // console.log("the webflow container", svgContainer);
     svgContainer.appendChild( svg );
     
-    // then add the event listeners
-    // detect when mouse is over item
     
     
   })
@@ -27,43 +24,58 @@ d3.xml( overlay )
   .then( data => {
     const svg = document.body.querySelector("#overlay-item");
 
+    // get the info
+    const info = getInfo();
+    console.log("chweck it out: ", info);
+
     // get the buttons
-    const buttons = svg.querySelectorAll(".button");
+    const buttonsDom = svg.querySelectorAll(".button");
+    const buttons = [...buttonsDom];
 
     // get the blur image
     const blurLayer = svg.querySelectorAll(".svg-blur-image");
     // console.log("buttons", blurLayer)
     
-
-    buttons.forEach((element) => {
-      console.log("look at the e", element.id)
+    // then add the event listeners
+    // detect when mouse is over item
+    info.forEach( ( element ) => {
+      // match the button to the info id
+      console.log("buttins", buttons)
+      const match = buttons.find((e) => e.id === element.idMatch);
       
-      element.addEventListener("mouseover", () => {
+      console.log("match it: ", match);
+
+      match.addEventListener("mouseover", () => {
         // console.log(`mouse over: ${element.id} `);
         
         // get the id and replace the mask url
         blurLayer.forEach((e) => {
-          console.log("hmmm",`url(#${element.id})` )
-          e.setAttribute('mask', `url(#mask-${element.id})`);
+          // console.log("hmmm",`url(#${match.id})` )
+          e.setAttribute('mask', `url(#mask-${match.id})`);
         });
         
         // make the blur layer visible, with an ease animation in css
         blurLayer.forEach((e) => {
           e.style.opacity = "100";
         });
+
+        // make the card visible
+        card.style.display = "block"
+        card.innerText = element.body;
       });
       
-      element.addEventListener("mouseout", () => {
-        console.log(`mouse off: ${element.id}`);
+      match.addEventListener("mouseout", () => {
+        // console.log(`mouse off: ${match.id}`);
         
         // make the blur layer dissapear with an eas out in css
         blurLayer.forEach((e) => {
           e.style.opacity = "0";
         });
-      });
 
-    }); 
-
+        card.style.display = "none";
+      }); 
+    });
+    
   }
 
 )
